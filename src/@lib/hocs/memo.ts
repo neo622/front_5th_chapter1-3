70b1e287cb0b6bef7308407399d70609d128a297
ghlee <94736572+neo622@users.noreply.github.com>
2025-04-09 23:32:02 +0900

    feat: memo hoc 구현

--- src/@lib/hocs/memo.ts
+++ src/@lib/hocs/memo.ts
@@ -1,10 +1,26 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { shallowEquals } from "../equalities";
-import { ComponentType } from "react";
+import { FunctionComponent, ReactNode } from "react";
+import { useRef } from "../hooks";
 
 export function memo<P extends object>(
-  Component: ComponentType<P>,
-  _equals = shallowEquals,
+  Component: FunctionComponent<P>,
+  equals = shallowEquals
 ) {
-  return Component;
+  return function MemoizedComponent(props: P): ReactNode {
+    const previous = useRef<{
+      props: P;
+      result: ReactNode;
+    } | null>(null);
+
+    // 1. 이전 props가 존재하고, 현재와 같으면 이전 결과 재사용
+    if (previous.current && equals(previous.current.props, props)) {
+      return previous.current.result;
+    }
+
+    // 2. 새로 계산한 결과 저장
+    const result = Component(props);
+    previous.current = { props, result };
+    return result;
+  };
 }
--- src/@lib/hooks/useMemo.ts
+++ src/@lib/hooks/useMemo.ts
@@ -19,7 +19,7 @@ export function useMemo<T>(
     initialized: false, // 처음 렌더인지 확인하는 플래그
   }); // deps와 value를 기억하는 객체 -> useRef를 이용해서 컴포넌트가 리랜더링되더라도 같은 참조를 유지함
 
-  console.log("memoRef", memoRef);
+  // console.log("memoRef", memoRef);
 
   if (!memoRef.current.initialized || !_equals(memoRef.current.deps, _deps)) {
     memoRef.current.value = factory(); //deps가 바뀌었으면 factory()를 실행해서 value를 다시 계산하고 저장
